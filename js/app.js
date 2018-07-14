318a50ac0edee391d972f5ffb052aed0a3b789b6
Mostafa Adel Abou-Salama <abousalama.mostafa@gmail.com>
2018-07-14 22:34:08 +0200

    Add trimmed versions of the characters pngs and basic modifications in app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function (x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
+    this.xPosition = x;
+    this.yPosition = y;
+    this.enemySpeed = speed;
+
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = 'images/enemy-bug-trim.png';
 };
 
 // Update the enemy's position, required method for game
@@ -14,21 +18,53 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.xPosition += this.enemySpeed * dt;
+
+    // Condition to detect collisions
+    // 67 and 88 are the width and height values of the player
+    // 99 and 77 are the width and height values of the bug (i.e enemy)
+    if (this.xPosition < player.xPosition + 67 && this.xPosition + 99 > player.xPosition &&
+        this.yPosition < player.yPosition + 88 && 77 + this.yPosition > player.yPosition) {
+          // Reset the player
+
+    }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.xPosition, this.yPosition);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function (x, y) {
+  this.xPosition = x;
+  this.yPosition = y;
+  this.sprite = 'images/char-boy-trim.png';
+}
+
+Player.prototype.update = function () {
+
+}
+
+Player.prototype.render = function () {
+  ctx.drawImage(Resources.get(this.sprite), this.xPosition, this.yPosition);
+}
+
+Player.prototype.handleInput = function (move) {
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+// const enemy1 = new Enemy( 0, 53+83, 500);
+// const enemy2 = new Enemy( 0, 53+83, 250);
+const enemy3 = new Enemy( 0, 53+83, 125);
+const allEnemies = [enemy3];
 // Place the player object in a variable called player
+const player = new Player(17+100+100+100, 45+83);
 
 
 
