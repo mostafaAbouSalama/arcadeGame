5ae85071e329b316f02b02e322853c3d62a66ed0
Mostafa Adel Abou-Salama <abousalama.mostafa@gmail.com>
2018-07-15 22:26:56 +0200

    Full game functionality complete

--- js/app.js
+++ js/app.js
@@ -20,12 +20,19 @@ Enemy.prototype.update = function(dt) {
     // all computers.
     this.xPosition += this.enemySpeed * dt;
 
+    if (this.xPosition >= 505) {
+      this.xPosition = -99;
+      this.enemySpeed = getRandomInteger(75, 400); // Set a new random speed for the bug after it crosses the canvas width
+    }
+
     // Condition to detect collisions
-    // 67 and 88 are the width and height values of the player
-    // 99 and 77 are the width and height values of the bug (i.e enemy)
+    // 67 and 76 are the width and height values of the player after discarding the shadow of character (actual chracter body)
+    // 99 and 66 are the width and height values of the bug (i.e enemy) after discarding the shadow of the bug (actual bug body)
     if (this.xPosition < player.xPosition + 67 && this.xPosition + 99 > player.xPosition &&
-        this.yPosition < player.yPosition + 88 && 77 + this.yPosition > player.yPosition) {
-          // Reset the player
+        this.yPosition < player.yPosition + 76 && 66 + this.yPosition > player.yPosition) {
+          // Reset the player position
+          player.xPosition = 219;
+          player.yPosition = 464;
 
     }
 };
@@ -54,19 +61,62 @@ Player.prototype.render = function () {
 }
 
 Player.prototype.handleInput = function (move) {
+  if (modal.style.display !== "block") { // Condition checks when the user has won so it will not move the player position until he/she responds to the modal
+    switch (move) {
+      case "up":
+            if (this.yPosition === 49) {
+              break;
+            }
+            this.yPosition -= 83;
+            break;
+      case "down":
+            if (this.yPosition === 464) {
+              break;
+            }
+            this.yPosition += 83;
+            break;
+      case "right":
+            if (this.xPosition === 421){
+              break;
+            }
+            this.xPosition += 101;
+            break;
+      case "left":
+            if (this.xPosition === 17) {
+              break;
+            }
+            this.xPosition -= 101;
+            break;
+    }
+  }
+}
 
+Player.prototype.reset = function (position) {
+    if (position === 49) {
+        modal.style.display = "block";
+    }
+}
+
+// Function returns a random number to use as speed for bugs
+function getRandomInteger(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-// const enemy1 = new Enemy( 0, 53+83, 500);
-// const enemy2 = new Enemy( 0, 53+83, 250);
-const enemy3 = new Enemy( 0, 53+83, 125);
-const allEnemies = [enemy3];
+const enemy1 = new Enemy( -99, 138, getRandomInteger(75, 400)); //
+const enemy2 = new Enemy( -99, 221, getRandomInteger(75, 400));
+const enemy3 = new Enemy( -99, 304, getRandomInteger(75, 400));
+const enemy4 = new Enemy( -99, 138, getRandomInteger(75, 400));
+const enemy5 = new Enemy( -99, 221, getRandomInteger(75, 400));
+const enemy6 = new Enemy( -99, 304, getRandomInteger(75, 400));
+const allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
 // Place the player object in a variable called player
-const player = new Player(17+100+100+100, 45+83);
+const player = new Player(219, 464);
 
-
+const modal = document.getElementById("theModal");
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -79,4 +129,23 @@ document.addEventListener('keyup', function(e) {
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
+    player.reset(player.yPosition);
+});
+
+document.querySelector("#playAgain").addEventListener("click", function() {
+    modal.style.display = "none";
+    player.xPosition = 219;
+    player.yPosition = 464;
+    allEnemies.forEach(function (ele, ind, arr) {
+      arr[ind].xPosition = -99;
+    });
+});
+
+document.querySelector(".close").addEventListener("click", function() {
+    modal.style.display = "none";
+    player.xPosition = 219;
+    player.yPosition = 464;
+    allEnemies.forEach(function (ele, ind, arr) {
+      arr[ind].xPosition = -99;
+    });
 });
